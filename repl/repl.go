f2d90ad3645053c2cdd810dcd6c03562cbc41dca
Sebastian Zapata <pansezapata@gmail.com>
2026-08-14 19:00:57 -0500

    Limpieza del main: se dejan solo Lexer y REPL para enseñanza paso a paso

--- repl/repl.go
+++ repl/repl.go
@@ -2,10 +2,7 @@ package repl
 
 import (
 	"bufio"
-	"compigo/evaluator"
 	"compigo/lexer"
-	"compigo/object"
-	"compigo/parser"
 	"compigo/token"
 	"fmt"
 	"io"
@@ -14,15 +11,8 @@ import (
 
 const PROMPT = ">> "
 
-const (
-	LEXER_MODE = iota
-	PARSER_MODE
-	EVALUATOR_MODE
-)
-
-func Start(in io.Reader, out io.Writer, mode int) {
+func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -41,70 +31,8 @@ func Start(in io.Reader, out io.Writer, mode int) {
 
 		l := lexer.New(line)
 
-		if mode == LEXER_MODE {
-			// Modo 1: Solo imprimir Tokens
-			for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-				fmt.Fprintf(out, "%+v\n", tok)
-			}
-		} else if mode == PARSER_MODE {
-			// Modo 2: Construir e imprimir el AST
-			p := parser.New(l)
-			program := p.ParseProgram()
-			
-			if len(p.Errors()) != 0 {
-				printParserErrors(out, p.Errors())
-				continue
-			}
-
-			// Imprimir la representación en string del AST
-			fmt.Fprintf(out, "AST Generado: \n")
-			fmt.Fprintf(out, "%s\n", program.String())
-		} else {
-			// Modo 3: Evaluador
-			p := parser.New(l)
-			program := p.ParseProgram()
-			if len(p.Errors()) != 0 {
-				printParserErrors(out, p.Errors())
-				continue
-			}
-
-			evaluated := evaluator.Eval(program, env)
-			if evaluated != nil {
-				fmt.Fprintf(out, "%s\n", evaluated.Inspect())
-			}
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
 		}
-	}
-}
-
-func printParserErrors(out io.Writer, errors []string) {
-	fmt.Fprintf(out, "¡Miau! Ocurrió un error al intentar parsear el código:\n")
-	for _, msg := range errors {
-		fmt.Fprintf(out, "\t- %s\n", msg)
-	}
-}
-
-func StartFile(in io.Reader, out io.Writer) {
-	scanner := bufio.NewScanner(in)
-	env := object.NewEnvironment()
-	
-	// Leer todo el archivo en memoria
-	var code strings.Builder
-	for scanner.Scan() {
-		code.WriteString(scanner.Text())
-		code.WriteString("\n")
-	}
-
-	l := lexer.New(code.String())
-	p := parser.New(l)
-	program := p.ParseProgram()
-
-	if len(p.Errors()) != 0 {
-		printParserErrors(out, p.Errors())
-		return
-	}
-
-	evaluated := evaluator.Eval(program, env)
-	if evaluated != nil {
-		fmt.Fprintf(out, "%s\n", evaluated.Inspect())
 	}
 }
